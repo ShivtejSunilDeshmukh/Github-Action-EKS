ef38fc7218428208630deae818ad1b6e33b12011
Shivtej Deshmukh <shivtej.deshmukh@dataeconomy.ai>
2026-09-08 12:52:39 +0530

    Added Terraform Modules

--- terraform/var.tf
+++ terraform/var.tf
@@ -7,5 +7,39 @@ variable "aws_region" {
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = "github-actions-eks-demo"
+  default     = "ssd-github-actions-eks"
+}
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "ssd-github-actions-eks"
+}
+
+variable "cluster_version" {
+  type = string
+  default = "1.36"
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.xlarge"]
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 3
 }
